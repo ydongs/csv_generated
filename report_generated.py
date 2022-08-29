6cd849fa7af78582a1612045933e96f60b3a0f13
ydongs <edns@alumni.tongji.edu.cn>
2022-08-30 00:24:18 +0800

    update

--- report_generated.py
+++ report_generated.py
@@ -1,37 +1,45 @@
 import csv
 
-auto_file = open('summary_auto_channels_last_jit.log', 'r')
-manual_file = open('summary_manual_channels_last_jit.log', 'r')
-wo_file = open('summary_without_channels_last_jit.log', 'r')
+auto_jit = open('summary_auto_channels_last_jit.log', 'r')
+manual_jit = open('summary_manual_channels_last_jit.log', 'r')
+wo_jit = open('summary_without_channels_last_jit.log', 'r')
 
+auto_eager = open('summary_auto_channels_last_eager.log', 'r')
+manual_eager = open('summary_manual_channels_last_eager.log', 'r')
+wo_eager = open('summary_without_channels_last_eager.log', 'r')
 
-def generated_report(auto, manual, wo):
-    with open('fp32_inference_perf_report.csv', 'r+', newline='') as report_generated:
+def generated_report(auto, manual, wo, option):
+    with open('fp32_inference_perf'+option+'_report.csv', 'r+', newline='') as report_generated:
         writer = csv.writer(report_generated)
         writer.writerow(
-            ['model', 'auto_jit', 'manual_jit', 'without_jit', 'auto_vs_wo_speedup', 'auto_vs_manual_%diff'])
-        model = []
-        auto_jit = []
-        manual_jit = []
-        without_jit = []
+            ['model', 'auto_'+option, 'manual_'+option, 'without_'+option, 'auto_vs_wo_speedup', 'auto_vs_manual_%diff'])
+        model_ = []
+        auto_ = []
+        manual_ = []
+        without_ = []
         for line in auto:
             line = line.split()
-            model.append(line[1])
-            auto_jit.append(line[5])
+            model_.append(line[1])
+            auto_.append(line[5])
 
         for line in manual:
             line = line.split()
-            manual_jit.append(line[5])
+            manual_.append(line[5])
 
         for line in wo:
             line = line.split()
-            without_jit.append(line[5])
+            without_.append(line[5])
 
-        for i in range(len(model)):
-            auto_vs_wo_speedup = '%.6f' % (float(auto_jit[i]) / float(without_jit[i]))
-            auto_vs_manual_diff = '%.6f' % abs((float(manual_jit[i]) - float(without_jit[i])) / float(without_jit[i]))
-            lists = [model[i], auto_jit[i], manual_jit[i], without_jit[i], auto_vs_wo_speedup, auto_vs_manual_diff]
+        for i in range(len(model_)):
+            if float(without_[i])==0:
+                auto_vs_wo_speedup='x'
+                auto_vs_manual_diff='x'
+            else:
+                auto_vs_wo_speedup = '%.6f' % (float(auto_[i]) / float(without_[i]))
+                auto_vs_manual_diff = '%.6f' % abs((float(manual_[i]) - float(without_[i])) / float(without_[i]))
+            lists = [model_[i], auto_[i], manual_[i], without_[i], auto_vs_wo_speedup, auto_vs_manual_diff]
             writer.writerows([lists])
 
 
-generated_report(auto_file, manual_file, wo_file)
+generated_report(auto_jit, manual_jit, wo_jit,'jit')
+generated_report(auto_eager, manual_eager, wo_eager,'eager')
